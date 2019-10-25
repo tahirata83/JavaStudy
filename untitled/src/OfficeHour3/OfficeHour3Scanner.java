f05f8ed97d209be3d5a4420237ba89dd88caa2a9
tahirata <tahiratayev@gmail.com>
2019-10-25 04:53:16 +0300

    hey it is coming 333

---
+++ untitled/src/OfficeHour3/OfficeHour3Scanner.java
@@ -0,0 +1,39 @@
+package OfficeHour3;
+
+import java.util.Scanner;
+
+public class OfficeHour3Scanner {
+
+    public static void main(String[] args) {
+
+        System.out.println("What is your name?");
+        Scanner scan = new Scanner(System.in);
+
+        String s1 = scan.nextLine();
+        System.out.println(" Your name is--> "+s1);
+
+
+        System.out.println("How many car do you have?");
+        Scanner scan1 = new Scanner(System.in);
+
+        String s2 = scan.nextLine();
+        System.out.println(" Your name is--> "+s2);
+        System.out.println(" ==============================================================================> ");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
