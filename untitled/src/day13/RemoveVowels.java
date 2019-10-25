63b62a1d32e0098516d591c5ff63f1cd0a4a80df
tahirata <tahiratayev@gmail.com>
2019-10-25 04:57:56 +0300

    hey it is coming11111

---
+++ untitled/src/day13/RemoveVowels.java
@@ -0,0 +1,19 @@
+package day13;
+     class ReplaceVowels {
+
+    public static void main(String[] args) {
+//        iWantToCallSomeMethod(a);
+//        RemoveWovelsSlow();
+        String input = "TechnoStudy";
+        int N = input.length();
+
+        //input TechnoStudy
+        //output TXchXStXdX
+        char x = input.charAt(4);
+//        System.out.println("A" + "B");
+        iWantToCallSomeMethod(input);
+    }
+
+         private static void iWantToCallSomeMethod(String input) {
+         }
+     }
