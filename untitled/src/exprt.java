a3f30e024e7bcff95c68084edeeb5c608182b41d
tahirata <tahiratayev@gmail.com>
2019-10-25 04:23:21 +0300

    hey it is coming

---
+++ untitled/src/exprt.java
@@ -0,0 +1,41 @@
+class exprt {
+    //what is the output here
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int max = (b>=a) ? b : a;
+        System.out.println(max);
+
+
+//find out min number between two inputs, use user input and ternary operator
+    int val1 = 10;
+    int val2 = 20;
+//
+    int min =(val1>=val2) ? val1 : val2;
+        System.out.println(max);
+//    output
+//    10
+
+//find out absolute value between two inputs, use user input and ternary operator
+//    int val1 = -20;
+//
+//    int abs =
+//    output
+//    10
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }}
+
