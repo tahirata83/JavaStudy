cb0eccc91cde64c0e4098c90bc8aa7fc909386a0
tahirata <tahiratayev@gmail.com>
2019-10-25 04:49:20 +0300

    heeyyyyyy

---
+++ untitled/src/OfficeHour3/OfficeHour3Random.java
@@ -0,0 +1,25 @@
+package OfficeHour3;
+
+import java.util.Random;
+
+public class OfficeHour3Random {
+    public static void main(String[] args) {
+        Random rnd = new Random();
+        int i1 = rnd.nextInt();
+        System.out.println(" random number i1 is--> " + i1);
+
+        int i2 = rnd.nextInt();
+        System.out.println(" random number i2 is--> " + i2);
+
+
+        double d1 = rnd.nextDouble();
+        System.out.println(" random d1  is--> " + d1);
+
+
+
+
+    }
+
+
+    }
+
